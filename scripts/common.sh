59df74c463e6f9cce63e798e538eb627ac0ae6be
ukorvl <ukorotovskiy@gmail.com>
2026-03-23 16:13:17 +0530

    chore: integrate prettier-plugin-sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,8 +4,8 @@
 
 set -euo pipefail
 
-SCRIPT_DIR="${SCRIPT_DIR:-$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)}"
-REPO_ROOT=$(cd -- "$SCRIPT_DIR/.." &>/dev/null && pwd)
+SCRIPT_DIR="${SCRIPT_DIR:-$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)}"
+REPO_ROOT=$(cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd)
 LIB_PATH="lib"
 LIB_PACKAGE_JSON="$REPO_ROOT/$LIB_PATH/package.json"
 ROOT_PACKAGE_JSON="$REPO_ROOT/package.json"
@@ -15,7 +15,7 @@ EXAMPLES_PATH="example"
 check_command() {
   echo "Checking if command '$1' is available..."
 
-  if ! command -v "$1" >/dev/null 2>&1; then
+  if ! command -v "$1" > /dev/null 2>&1; then
     echo "❌ Error: Required command '$1' is not installed or not in PATH." >&2
     exit 1
   fi
